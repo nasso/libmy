35b5d8dec61c62543c38d0f7a65228a0697c0ac1
nasso <nassomails@gmail.com>
2019-11-11 15:42:46 +0100

    fix: rename formatting test suite

--- tests/format.c
+++ tests/format.c
@@ -8,35 +8,35 @@
 #include <criterion/criterion.h>
 #include "my.h"
 
-Test(my_printf, invalid_specifier)
+Test(my_format, invalid_specifier)
 {
     cr_assert_str_eq(my_format( "<%k>"),  "<%k>");
     cr_assert_str_eq(my_format( "<%31.5k>"),  "<%31.5k>");
     cr_assert_str_eq(my_format( "<%31.5k> %d", 42),  "<%31.5k> 42");
 }
 
-Test(my_printf, simple_no_args)
+Test(my_format, simple_no_args)
 {
     char *str = my_format("Hello World!");
 
     cr_assert_str_eq(str, "Hello World!");
 }
 
-Test(my_printf, multiple)
+Test(my_format, multiple)
 {
     char *str = my_format("%s %s!", "Hello", "World");
 
     cr_assert_str_eq(str, "Hello World!");
 }
 
-Test(my_printf, simple_string)
+Test(my_format, simple_string)
 {
     char *str = my_format("%s World!", "Hello");
 
     cr_assert_str_eq(str, "Hello World!");
 }
 
-Test(my_printf, simple_decimal)
+Test(my_format, simple_decimal)
 {
     cr_assert_str_eq(my_format( "<%d>",  42),  "<42>");
     cr_assert_str_eq(my_format( "<%d>", -42), "<-42>");
