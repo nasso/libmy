b0c7d5ce4912c2b9b525763ea994b85de624e697
nasso <nassomails@gmail.com>
2019-11-15 22:26:03 +0100

    fix: fmt: %s not padding correctly

--- src/fmt/my_fmt__converter_fn_s.c
+++ src/fmt/my_fmt__converter_fn_s.c
@@ -20,7 +20,7 @@ int my_fmt__converter_fn_s(my_fmt__converter_t *cv, bufwriter_t *bw, va_list ap)
         n = my_strlen(str);
     if (cv->flags->leftpad)
         bytes_written += bufwriter_write(bw, str, n);
-    for (int i = 0; i < cv->field_width - 1; i++)
+    for (int i = 0; i < cv->field_width - n; i++)
         bytes_written += bufwriter_putchar(bw, ' ');
     if (!cv->flags->leftpad)
         bytes_written += bufwriter_write(bw, str, n);
