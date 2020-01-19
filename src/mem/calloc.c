50e8c92e930821826cbd182170d9ecb31ee15db0
nasso <nassomails@gmail.com>
2020-01-19 20:59:34 +0100

    feat: use my_malloc is calloc isn't available

--- src/mem/calloc.c
+++ src/mem/calloc.c
@@ -6,18 +6,16 @@
 */
 
 #include <stdlib.h>
-#include "my.h"
+#include "mem.h"
 
 void *my_calloc(usize_t nmemb, usize_t size)
 {
     void *buf = NULL;
 
-    (void)(nmemb);
-    (void)(size);
 #if defined(MY_ALLOW_FUN_CALLOC)
     buf = calloc(nmemb, size);
-#elif defined(MY_ALLOW_FUN_MALLOC)
-    buf = malloc(nmemb * size);
+#else
+    buf = my_malloc(nmemb * size);
     if (buf != NULL)
         my_memset(buf, 0, nmemb * size);
 #endif
