ff2fb78e765b944c7b228d1ef9dbd34cc4faba7c
nasso <nassomails@gmail.com>
2020-01-19 14:52:41 +0100

    feat: add djb2 and sdbm hashing functions

---
+++ include/hash.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** libmy
+** File description:
+** Hashing algorithms
+*/
+
+#ifndef LIBMY_HASH_H
+#define LIBMY_HASH_H
+
+#include "types.h"
+
+u64_t my_djb2(const void*, usize_t);
+u64_t my_djb2_cstr(const char*);
+u64_t my_sdbm(const void*, usize_t);
+u64_t my_sdbm_cstr(const char*);
+
+#endif /* LIBMY_HASH_H */
