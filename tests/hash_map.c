bccc0a6b3d6974e5d8c1f29edfa11e13855683c2
nasso <nassomails@gmail.com>
2020-01-22 15:24:11 +0100

    test: add hash_map test with a lot of values

--- tests/hash_map.c
+++ tests/hash_map.c
@@ -6,6 +6,7 @@
 */
 
 #include <criterion/criterion.h>
+#include <stdlib.h>
 #include "my.h"
 #include "cstr.h"
 #include "collections/hash_map.h"
@@ -25,6 +26,16 @@ static int pair_cmp(void *a, void *b)
     hash_map_pair_t *pb = b;
 
     return (!my_cstrcmp(pa->key, pb->key) && !my_cstrcmp(pa->value, pb->value));
+}
+
+static char *randstr(usize_t n)
+{
+    char *str = my_calloc(sizeof(char), n + 1);
+
+    for (usize_t i = 0; i < n; i++)
+        str[i] = 'a' + (rand() % 27);
+    str[n] = '\0';
+    return (str);
 }
 
 Test(hash_map, new)
@@ -186,4 +197,22 @@ Test(hash_map, destroy_with)
     cr_assert_eq(ls->len, 2);
     cr_assert_not_null(list_find_with(ls, &pair_cmp, &pairs[0]));
     cr_assert_not_null(list_find_with(ls, &pair_cmp, &pairs[1]));
+}
+
+Test(hash_map, lots_of_values)
+{
+    char *keys[10000] = {NULL};
+    u8_t values[10000] = {0};
+    hash_map_t *map = hash_map_new();
+
+    for (usize_t i = 0; i < 10000; i++) {
+        keys[i] = randstr(64);
+        values[i] = i;
+        hash_map_insert(map, keys[i], &values[i]);
+    }
+    for (usize_t i = 0; i < 10000; i++)
+        cr_assert_eq(hash_map_get(map, keys[i]), &values[i]);
+    for (usize_t i = 0; i < 10000; i++)
+        my_free(keys[i]);
+    hash_map_destroy(map);
 }
